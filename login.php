95f1052515a769890016bbe6f2b2288089798ab6
Master Putra Praja <43258301+masterput@users.noreply.github.com>
2021-12-02 20:55:00 +0700

    add atribute name

--- login.php
+++ login.php
@@ -25,11 +25,11 @@ require('loginproses.php');
               <form action="" method="POST">
                 <div class="mb-4">
                   <label for="email" class="form-label">Email*</label>
-                  <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Your Email">
+                  <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" placeholder="Your Email">
                 </div>
                 <div class="mb-3">
                   <label for="password" class="form-label">Password*</label>
-                  <input type="password" class="form-control" id="password" placeholder="Min 8 character">
+                  <input type="password" class="form-control" id="password" name="password" placeholder="Min 8 character">
                 </div>
 
                 <div class="d-flex justify-content-between">
