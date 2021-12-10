ffc3cbbdce8a1274c3c1c466b7f6f578ead7dba5
Afifa Atira <76120310+arinnf@users.noreply.github.com>
2021-12-10 14:59:03 +0700

    Add config.php
    Menambahkan file config

---
+++ cat/config.php
@@ -0,0 +1,11 @@
+<?php 
+//mendefinisikan variabel
+$host = "localhost";
+$user = "root";
+$password = "";
+$dbname = "savekat_db";
+
+//membuat variabel koneksi untuk menyimpan nilai dari function mysqli connect
+$koneksi = mysqli_connect($host, $user, $password, $dbname);
+
+?>
