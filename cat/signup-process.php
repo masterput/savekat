b05a7af358e1b0c2c6e25129bb50d184b1dbb9be
Master Putra Praja <43258301+masterput@users.noreply.github.com>
2021-12-17 15:29:55 +0700

    update signup-process.php

--- cat/signup-process.php
+++ cat/signup-process.php
@@ -9,14 +9,19 @@ if(isset($_POST['register'])){
     $username = $_POST['username'];
     $email = $_POST['email'];
     $password = $_POST['password'];
+    $level = $_POST['level'];
 
     // fungsi enkripsi
     $encryptPassword = password_hash($password, PASSWORD_DEFAULT);
     // insert to database
-    $insert = mysqli_query($koneksi, "INSERT INTO user (nama, username, email, password) VALUES ('$name','$username','$email','$encryptPassword')");
+    $insert = mysqli_query($koneksi, "INSERT INTO user (nama, username, email, password, level) VALUES ('$name','$username','$email','$encryptPassword','$level')");
 
     if($insert) {
-        header('location:../login.php'); // redirect ke halaman login
+        echo '
+        <script>
+        alert("Registrasi Berhasil");
+        window.location.href="login.php";
+        </script>';
     } else {
         echo '
         <script>
