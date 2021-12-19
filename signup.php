4cf23177ca6a46385f4e6cc3cc7f577a51a42e7e
Afifa Atira <76120310+arinnf@users.noreply.github.com>
2021-12-19 14:46:16 +0700

    Update sign up
    Merapihkan kode

--- signup.php
+++ signup.php
@@ -1,14 +1,12 @@
 <?php
 require('cat/signup-process.php');
 ?>
+
 <!doctype html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="style/style.css">
     <link rel="icon" href="./img/logo/SaveKat-Icon(White).png" type="image/png">
@@ -20,7 +18,6 @@ require('cat/signup-process.php');
             <div class="card-body">
               <h2 class="card-title">Sign-Up</h2>
               <h6 class="card-subtitle text-muted mb-4 mt-2 fw-bold">Please sign-up to use this site!</h6>
-
               <form action="" method="POST">
                 <div class="input-signup">
                   <div class="mb-3">
