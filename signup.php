50c72ea85dddbdba99383842f3dddce955fc638f
Master Putra Praja <43258301+masterput@users.noreply.github.com>
2021-12-20 14:26:24 +0700

    update signup.php
    add input type

--- signup.php
+++ signup.php
@@ -4,8 +4,11 @@ require('cat/signup-process.php');
 <!doctype html>
 <html lang="en">
   <head>
+    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="style/style.css">
     <link rel="icon" href="./img/logo/SaveKat-Icon(White).png" type="image/png">
@@ -17,7 +20,6 @@ require('cat/signup-process.php');
             <div class="card-body">
               <h2 class="card-title">Sign-Up</h2>
               <h6 class="card-subtitle text-muted mb-4 mt-2 fw-bold">Please sign-up to use this site!</h6>
-
               <form action="" method="POST">
                 <div class="input-signup">
                   <div class="mb-3">
@@ -36,6 +38,7 @@ require('cat/signup-process.php');
                     <label for="password" class="form-label">Password*</label>
                     <input type="password" class="form-control" id="password" name="password" aria-label="Masukkan password anda" placeholder="Min 8 character" Required>
                   </div>
+                    <input type="hidden" value="user" name="level">
                 </div>
 
                 <div class="d-grid mt-4">
